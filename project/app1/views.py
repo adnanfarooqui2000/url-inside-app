811bd63d33a6aec87b35fb078e76362d34adb0d3
Adnan Farooqui <adnanfarooqui2000@gmail.com>
2026-03-23 06:07:29 +0530

    modified:   project/app1/__pycache__/views.cpython-312.pyc 	modified:   project/app1/views.py 	modified:   project/app2/__pycache__/views.cpython-312.pyc 	modified:   project/project/__pycache__/urls.cpython-312.pyc

--- project/app1/views.py
+++ project/app1/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+
 def home(request):
-    return HttpResponse("Welcome to App 1 page of django")
+    return HttpResponse("<center><h1 style='color : purple'>Welcome to App 1 page of django </h1></center`>")
+data={
+"Name":"Adnan Farooqui"
+"Age" "23"
+}
+def register(request):
+    return render(request,'register.html')
